759bbe67eead9342ae8b43f4cd3442dd43eca340
Anton <antonn.horoshko@gmail.com>
2025-04-01 21:20:33 +0200

    Added main menu

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    public static void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public static void Quit()
+    {
+        Application.Quit();
+    }
+
+
+}
