d786abd5480b5fa894dd3a4a4c5339d5abe7865d
Anton <antonn.horoshko@gmail.com>
2025-04-19 23:58:33 +0200

    add transition

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public Animator transition;
+    public float TransitionTime = 1f;
 
-    public static void LoadScene(int sceneIndex)
+    public void LoadScene(int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
+        StartCoroutine(Transition(sceneIndex));
+
+
     }
 
-    public static void Quit()
+    public void Quit()
     {
         Application.Quit();
+    }
+
+    IEnumerator Transition(int sceneIndex)
+    {
+        transition.SetTrigger("Play");
+
+        yield return new WaitForSeconds(TransitionTime);
+
+        SceneManager.LoadScene(sceneIndex);
     }
 }
